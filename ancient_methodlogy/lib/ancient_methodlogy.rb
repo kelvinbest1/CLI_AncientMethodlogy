447ee1a85316b43ab759b5e50f0545d1bc1c4d39
kelvinbest1 <60621382+kelvinbest1@users.noreply.github.com>
2020-08-22 23:47:57 -0400

    Update and rename ancient_methodlogy.rb to environment.rb

--- ancient_methodlogy/lib/ancient_methodlogy.rb
+++
@@ -1,6 +0,0 @@
-require "ancient_methodlogy/version"
-
-module AncientMethodlogy
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ ancient_methodlogy/lib/environment.rb
@@ -0,0 +1,8 @@
+require_relative "./ancient_mythology/version"
+require_relative "./ancient_mythology/cli"
+require_relative "./ancient_mythology/god"
+require_relative "./ancient_mytholody/scraper"
+require "nokogiri"
+require "open-uri"
+require "pry"
+© 2020 GitHub, Inc.
