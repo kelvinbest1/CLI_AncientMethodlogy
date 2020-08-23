eaf77a7445117da4e729ab17e89dba3787170790
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-23 01:34:20 +0000

    first commit

---
+++ ancient_methodlogy/lib/ancient_methodlogy.rb
@@ -0,0 +1,6 @@
+require "ancient_methodlogy/version"
+
+module AncientMethodlogy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ ancient_methodlogy/lib/ancient_methodlogy/version.rb
@@ -0,0 +1,3 @@
+module AncientMethodlogy
+  VERSION = "0.1.0"
+end
