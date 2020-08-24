e0f98a9fce0819981fb42c7679577daa4c5000db
kelvinbest1 <60621382+kelvinbest1@users.noreply.github.com>
2020-08-23 22:33:17 -0400

    Update environment.rb

--- ancient_methodlogy/lib/environment.rb
+++ ancient_methodlogy/lib/environment.rb
@@ -1,4 +1,4 @@
-require_relative "./ancient_mythology/version"
+
 require_relative "./ancient_mythology/cli"
 require_relative "./ancient_mythology/god"
 require_relative "./ancient_mytholody/scraper"
