718ca38da23383b589beba90c27a23103682c8df
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-25 02:52:26 +0000

    extra

--- ancient_mythologies/lib/ancient_methodlogy/cli.rb
+++ ancient_mythologies/lib/ancient_methodlogy/cli.rb
@@ -1,5 +1,5 @@
-class AncientMythologies::CLI
-  
+class CLI
+
   def call
     AncientMythologies::Scraper.scrape_mythologies
     welcome
@@ -7,31 +7,31 @@ class AncientMythologies::CLI
     menu
     goodbye
   end
-  
+
   def welcome
 		puts ""
 		puts "----------------------------------------"
 		puts "            ANCIENT MYTHOLOGIES               "
 		puts "----------------------------------------"
   end
-  
+
  def display_mythologies
     Mythology.all.each.with_index(1) do |mythology, i|
       puts "#{i}.   #{mythology.name}"
     end
   end
-  
+
   def display_info
     Mythology.all.each.with_index(1) do |mythology, i|
     end
   end
-  
+
   def menu
     input = nil
     while input != "exit"
       puts "Enter the number of the mythologies you want more information on, type list to see list, or exit:"
       input = gets.strip.downcase
-      
+
      if input.to_i > 0 && input.to_i <8
         the_mythology = display_info[input.to_i-1]
         puts "#{the_mythology.name} - #{the_mythology.info}"
