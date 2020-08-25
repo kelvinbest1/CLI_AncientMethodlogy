2b962a0853c374611e82bdc8a6d279f22d9d061b
kelvinbest1 <60621382+kelvinbest1@users.noreply.github.com>
2020-08-24 22:30:28 -0400

    Update environment.rb

--- ancient_mythologies/lib/environment.rb
+++ ancient_mythologies/lib/environment.rb
@@ -1,7 +1,7 @@
 
-require_relative "./ancient_mythologies/cli"
-require_relative "./ancient_mythologies/mythologies"
-require_relative "./ancient_mythologies/scraper"
+require "./lib/ancient_mythologies/cli"
+require "./lib/ancient_mythologies/scraper"
+require "./lib/ancient_mythologies/mythologies"
 require "nokogiri"
 require "open-uri"
 require "pry"
